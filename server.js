87b484a9d19acde31de6ff112c4c51e8662ef72a
Ashita Gaur <microsmall87@gmail.com>
2019-05-04 01:19:07 +0530

    Deployed to heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/<badminton-video-analysis>'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/<badminton-video-analysis>/dashboard.component.html'));
+  res.sendFile(path.join(__dirname + '/dist/<badminton-video-analysis>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
